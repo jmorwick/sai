abd02a862267a4340f91ade293c95241eae236cb
Joey Kendall-Morwick <jbmorwick@gmail.com>
2014-06-03 22:04:09 -0500

    completed unit tests for basic db interface

--- SAI/test/sai/db/BasicDBInterfaceTest.java
+++ SAI/test/sai/db/BasicDBInterfaceTest.java
@@ -2,102 +2,430 @@ package sai.db;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.nio.file.AccessDeniedException;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.google.common.collect.Sets;
+
+import sai.graph.BasicGraphFactory;
+import sai.graph.Feature;
+import sai.graph.Graph;
+import sai.graph.GraphFactory;
+import sai.graph.Graphs;
+import sai.graph.MutableGraph;
+import sai.graph.SampleGraphs;
+import static sai.graph.SampleGraphs.assertGraphsAreIdentical;
+
 public class BasicDBInterfaceTest {
+	
+	private static Set<File> tempFiles;
 
 	@Before
 	public void setUp() throws Exception {
+		tempFiles = Sets.newHashSet();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		//delete all temporary files
+		for(File f : tempFiles)
+			f.delete();
 	}
 
 	@Test
-	public void testSavingToFile() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testLoadingFromFile() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddingGraph() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddingMultipleGraphs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddingMultipleSimilarGraphs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddingIndexToGraph() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAddingIndexToMultipleGraphs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMultipleIndicesMultipleGraphs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testHidingGraphs() {
-		fail("Not yet implemented");
-	}
-	@Test
-	public void testDeletingGraphs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGraphIterator() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGraphIDRetrieval() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIndexIterator() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIndexIDRetrieval() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testBasicFeatureMethods() {
-		fail("Not yet implemented");
+	public void testSavingToAndLoadingFromFileNoGraphs() throws AccessDeniedException {
+		BasicDBInterface db = new BasicDBInterface(new BasicGraphFactory());
+		File f = new File("/tmp/sai-db-test-"+Math.random());
+		tempFiles.add(f);
+		if(f.exists()) f.delete();
+		db.connect();
+		db.setDBFile(f);
+		assertEquals(0, db.getDatabaseSize());
+		db.disconnect();
+		db.connect();
+		assertEquals(0, db.getDatabaseSize());
 	}
 	
 	@Test
-	public void testFeatureCompatability() {
-		fail("Not yet implemented");
+	public void testSavingToAndLoadingFromFileOneGraph() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		File f = new File("/tmp/sai-db-test-"+Math.random());
+		tempFiles.add(f);
+		if(f.exists()) f.delete();
+		db.connect();
+		db.setDBFile(f);
+		assertEquals(0, db.getDatabaseSize());
+		int gid = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		assertEquals(1, db.getDatabaseSize());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid, gf));
+		
+		db.disconnect();
+		
+
+		db = new BasicDBInterface(gf);
+		db.setDBFile(f);
+		db.connect();
+		assertEquals(1, db.getDatabaseSize());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid, gf));
 	}
 
 	@Test
-	public void testDatabaseSizeMethods() {
-		fail("Not yet implemented");
+	public void testSavingAndLodaingMultipleGraphs() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		File f = new File("/tmp/sai-db-test-"+Math.random());
+		tempFiles.add(f);
+		if(f.exists()) f.delete();
+		db.connect();
+		db.setDBFile(f);
+		assertEquals(0, db.getDatabaseSize());
+		assertEquals(Sets.newHashSet(), db.getFeatureIDs());
+		int gid1 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		int gid2 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		int gid3 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+
+		assertEquals(3, db.getDatabaseSize());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid1, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph2(db), 
+				db.retrieveGraph(gid2, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid3, gf));
+		
+		db.disconnect();
+		
+
+		db = new BasicDBInterface(gf);
+		db.setDBFile(f);
+		db.connect();
+		assertEquals(3, db.getDatabaseSize());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid1, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph2(db), 
+				db.retrieveGraph(gid2, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid3, gf));
+	}
+
+	@Test
+	public void testAddingIndexToGraph() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		File f = new File("/tmp/sai-db-test-"+Math.random());
+		tempFiles.add(f);
+		if(f.exists()) f.delete();
+		db.connect();
+		db.setDBFile(f);
+		assertEquals(0, db.getDatabaseSize());
+		int gid1 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		int gid2 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		MutableGraph i = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		i.addFeature(Graphs.getIndexTag(db));
+		int gid3 = db.addGraph(i);
+		db.addIndex(gid1, gid3);
+
+		assertEquals(3, db.getDatabaseSize());
+		assertEquals(2, db.getDatabaseSizeWithoutIndices());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4, 5), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid1, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph2(db), 
+				db.retrieveGraph(gid2, gf));
+		MutableGraph rg = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		rg.addFeature(Graphs.getIndexTag(db));
+		assertGraphsAreIdentical(rg, 
+				db.retrieveGraph(gid3, gf));
+		assertEquals(gid3, (int)db.getIndexIDIterator().next());
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid2));
+		assertEquals(Sets.newHashSet(gid1), db.retrieveIndexedGraphIDs(gid3));
+		assertEquals(Sets.newHashSet(gid3), db.retrieveIndexIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid2));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid3));
+		Iterator<Integer> ii = db.getIndexIDIterator();
+		assertTrue(ii.hasNext());
+		assertEquals(gid3, (int)ii.next());
+		assertTrue(!ii.hasNext());
+		db.disconnect();
+		
+
+		db = new BasicDBInterface(gf);
+		db.setDBFile(f);
+		db.connect();
+		assertEquals(3, db.getDatabaseSize());
+		assertEquals(2, db.getDatabaseSizeWithoutIndices());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4, 5), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid1, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph2(db), 
+				db.retrieveGraph(gid2, gf));
+		rg = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		rg.addFeature(Graphs.getIndexTag(db));
+		assertGraphsAreIdentical(rg, 
+				db.retrieveGraph(gid3, gf));
+		assertEquals(gid3, (int)db.getIndexIDIterator().next());
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid2));
+		assertEquals(Sets.newHashSet(gid1), db.retrieveIndexedGraphIDs(gid3));
+		assertEquals(Sets.newHashSet(gid3), db.retrieveIndexIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid2));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid3));
+		ii = db.getIndexIDIterator();
+		assertTrue(ii.hasNext());
+		assertEquals(gid3, (int)ii.next());
+		assertTrue(!ii.hasNext());
+	}
+
+	@Test
+	public void testAddingIndexToMultipleGraphs() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		File f = new File("/tmp/sai-db-test-"+Math.random());
+		tempFiles.add(f);
+		if(f.exists()) f.delete();
+		db.connect();
+		db.setDBFile(f);
+		assertEquals(0, db.getDatabaseSize());
+		int gid1 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		int gid2 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		MutableGraph i1 = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		i1.addFeature(Graphs.getIndexTag(db));
+		int gid3 = db.addGraph(i1);
+		db.addIndex(gid1, gid3);
+		MutableGraph i2 = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		i2.addFeature(Graphs.getIndexTag(db));
+		int gid4 = db.addGraph(i2);
+		db.addIndex(gid1, gid4);
+		db.addIndex(gid3, gid4);
+
+		assertEquals(4, db.getDatabaseSize());
+		assertEquals(2, db.getDatabaseSizeWithoutIndices());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4, 5), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid1, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph2(db), 
+				db.retrieveGraph(gid2, gf));
+		MutableGraph rg = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		rg.addFeature(Graphs.getIndexTag(db));
+		assertGraphsAreIdentical(rg, 
+				db.retrieveGraph(gid3, gf));
+		assertGraphsAreIdentical(rg, 
+				db.retrieveGraph(gid4, gf));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid2));
+		assertEquals(Sets.newHashSet(gid1), db.retrieveIndexedGraphIDs(gid3));
+		assertEquals(Sets.newHashSet(gid1,gid3), db.retrieveIndexedGraphIDs(gid4));
+		assertEquals(Sets.newHashSet(gid3,gid4), db.retrieveIndexIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid2));
+		assertEquals(Sets.newHashSet(gid4), db.retrieveIndexIDs(gid3));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid4));
+		Iterator<Integer> ii = db.getIndexIDIterator();
+		assertTrue(ii.hasNext());
+		assertEquals(gid3, (int)ii.next());
+		assertTrue(ii.hasNext());
+		assertEquals(gid4, (int)ii.next());
+		assertTrue(!ii.hasNext());
+		db.disconnect();
+		
+
+		db = new BasicDBInterface(gf);
+		db.setDBFile(f);
+		db.connect();
+		assertEquals(4, db.getDatabaseSize());
+		assertEquals(2, db.getDatabaseSizeWithoutIndices());
+		assertEquals(Sets.newHashSet(1, 2, 3, 4, 5), db.getFeatureIDs());
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph1(db), 
+				db.retrieveGraph(gid1, gf));
+		assertGraphsAreIdentical(SampleGraphs.getSmallGraph2(db), 
+				db.retrieveGraph(gid2, gf));
+		rg = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		rg.addFeature(Graphs.getIndexTag(db));
+		assertGraphsAreIdentical(rg, 
+				db.retrieveGraph(gid3, gf));
+		assertGraphsAreIdentical(rg, 
+				db.retrieveGraph(gid4, gf));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexedGraphIDs(gid2));
+		assertEquals(Sets.newHashSet(gid1), db.retrieveIndexedGraphIDs(gid3));
+		assertEquals(Sets.newHashSet(gid1,gid3), db.retrieveIndexedGraphIDs(gid4));
+		assertEquals(Sets.newHashSet(gid3,gid4), db.retrieveIndexIDs(gid1));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid2));
+		assertEquals(Sets.newHashSet(gid4), db.retrieveIndexIDs(gid3));
+		assertEquals(Sets.newHashSet(), db.retrieveIndexIDs(gid4));
+	}
+
+	@Test
+	public void testGraphIterator() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		db.connect();
+		assertEquals(0, db.getDatabaseSize());
+		int gid1 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		int gid2 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		int gid3 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		int gid4 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		
+		Set<Integer> observed = Sets.newHashSet();
+		Set<Integer> total = Sets.newHashSet(gid1, gid2, gid3, gid4);
+		Iterator<Integer> gi = db.getGraphIDIterator();
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(4, Sets.union(total, observed).size());
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(4, Sets.union(total, observed).size());
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(4, Sets.union(total, observed).size());
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(4, Sets.union(total, observed).size());
+		assertTrue(!gi.hasNext());
+	}
+	
+	@Test
+	public void testIndexIterator() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		db.connect();
+		assertEquals(0, db.getDatabaseSize());
+		int gid1 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		int gid2 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		MutableGraph i1 = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		i1.addFeature(Graphs.getIndexTag(db));
+		int gid3 = db.addGraph(i1);
+		db.addIndex(gid1, gid3);
+		MutableGraph i2 = new MutableGraph(SampleGraphs.getSmallGraph1(db));
+		i2.addFeature(Graphs.getIndexTag(db));
+		int gid4 = db.addGraph(i2);
+		db.addIndex(gid1, gid4);
+		db.addIndex(gid3, gid4);
+		
+		Iterator<Integer> ii = db.getIndexIDIterator();
+		assertTrue(ii.hasNext());
+		assertEquals(gid3, (int)ii.next());
+		assertTrue(ii.hasNext());
+		assertEquals(gid4, (int)ii.next());
+		assertTrue(!ii.hasNext());
+	}
+
+	@Test
+	public void testHidingGraphs() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		db.connect();
+		assertEquals(0, db.getDatabaseSize());
+		int gid1 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		int gid2 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		int gid3 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		int gid4 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+
+		db.hideGraph(gid2);
+		db.hideGraph(gid3);
+		
+		Set<Integer> observed = Sets.newHashSet();
+		Set<Integer> total = Sets.newHashSet(gid1, gid4);
+		Iterator<Integer> gi = db.getGraphIDIterator();
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(2, Sets.union(total, observed).size());
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(2, Sets.union(total, observed).size());
+		assertTrue(!gi.hasNext());
+
+		db.unhideGraph(gid2);
+		db.hideGraph(gid4);
+		
+		observed = Sets.newHashSet();
+		total = Sets.newHashSet(gid1, gid2, gid3);
+		gi = db.getGraphIDIterator();
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(3, Sets.union(total, observed).size());
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(3, Sets.union(total, observed).size());
+		assertTrue(!gi.hasNext());
+	}
+	
+	@Test
+	public void testDeletingGraphs() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		db.connect();
+		assertEquals(0, db.getDatabaseSize());
+		int gid1 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		int gid2 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		int gid3 = db.addGraph(SampleGraphs.getSmallGraph2(db));
+		db.deleteGraph(gid3);
+		int gid4 = db.addGraph(SampleGraphs.getSmallGraph1(db));
+		db.hideGraph(gid2);
+		
+		Set<Integer> observed = Sets.newHashSet();
+		Set<Integer> total = Sets.newHashSet(gid1, gid4);
+		Iterator<Integer> gi = db.getGraphIDIterator();
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(2, Sets.union(total, observed).size());
+		assertTrue(gi.hasNext());
+		observed.add(gi.next());
+		assertEquals(2, Sets.union(total, observed).size());
+		assertTrue(!gi.hasNext());
+	}
+	
+	@Test
+	public void testBasicFeatureMethods() throws AccessDeniedException {
+		GraphFactory gf = new BasicGraphFactory();
+		BasicDBInterface db = new BasicDBInterface(gf);
+		db.connect();
+		
+		
+		assertEquals(Sets.newHashSet(), db.getFeatureIDs());
+		assertEquals(Sets.newHashSet(), db.getFeatureNames());
+		assertEquals(Sets.newHashSet(), db.getFeatureIDs("a"));
+		assertNull(db.getFeature(1));
+		
+		Feature f1 = db.getFeature("a", "1");
+		assertEquals(f1, db.getFeature(1));
+		assertEquals(f1, db.getFeature("a", "1"));
+		assertEquals(Sets.newHashSet(1), db.getFeatureIDs());
+		assertEquals(Sets.newHashSet("a"), db.getFeatureNames());
+		assertEquals(Sets.newHashSet(1), db.getFeatureIDs("a"));
+		
+		Feature f2 = db.getFeature("a", "2");
+		assertEquals(f1, db.getFeature(1));
+		assertEquals(f1, db.getFeature("a", "1"));
+		assertEquals(f2, db.getFeature(2));
+		assertEquals(f2, db.getFeature("a", "2"));
+		assertEquals(Sets.newHashSet(1,2), db.getFeatureIDs());
+		assertEquals(Sets.newHashSet("a"), db.getFeatureNames());
+		assertEquals(Sets.newHashSet(1,2), db.getFeatureIDs("a"));
+		
+		Feature f3 = db.getFeature("b", "1");
+		assertEquals(f1, db.getFeature(1));
+		assertEquals(f1, db.getFeature("a", "1"));
+		assertEquals(f2, db.getFeature(2));
+		assertEquals(f2, db.getFeature("a", "2"));
+		assertEquals(f3, db.getFeature(3));
+		assertEquals(f3, db.getFeature("b", "1"));
+		assertEquals(Sets.newHashSet(1,2,3), db.getFeatureIDs());
+		assertEquals(Sets.newHashSet("a","b"), db.getFeatureNames());
+		assertEquals(Sets.newHashSet(1,2), db.getFeatureIDs("a"));
+		assertEquals(Sets.newHashSet(3), db.getFeatureIDs("b"));
 	}
 
 }
---
+++ SAI/test/sai/graph/SampleGraphs.java
@@ -0,0 +1,109 @@
+package sai.graph;
+
+import static org.junit.Assert.assertEquals;
+import sai.db.DBInterface;
+import sai.graph.Graph;
+import sai.graph.MutableGraph;
+
+
+public class SampleGraphs {
+
+    public static Graph getSmallGraph1(DBInterface db) {
+      MutableGraph g = new MutableGraph();
+      g.addNode(1);
+      g.addNodeFeature(1, db.getFeature("test", "a"));
+      g.addNode(2);
+      g.addNodeFeature(2, db.getFeature("test", "b"));
+      g.addNode(3);
+      g.addNodeFeature(3, db.getFeature("test", "c"));
+      g.addNode(4);
+      g.addNodeFeature(4, db.getFeature("test", "d"));
+      g.addEdge(1, 1, 2);
+      g.addEdgeFeature(1, db.getFeature("test", "a"));
+      g.addEdge(2, 2, 3);
+      g.addEdgeFeature(2, db.getFeature("test", "a"));
+      g.addEdge(3, 3, 4);
+      g.addEdgeFeature(3, db.getFeature("test", "a"));
+      g.addEdge(4, 2, 4);
+      g.addEdgeFeature(4, db.getFeature("test", "a"));
+      return g;
+    }
+
+    public static Graph getSmallGraph2(DBInterface db) {
+        MutableGraph g = new MutableGraph();
+        g.addNode(1);
+        g.addNodeFeature(1, db.getFeature("test", "a"));
+        g.addNode(2);
+        g.addNodeFeature(2, db.getFeature("test", "b"));
+        g.addNode(3);
+        g.addNodeFeature(3, db.getFeature("test", "c"));
+        g.addNode(4);
+        g.addNodeFeature(4, db.getFeature("test", "d"));
+        g.addEdge(1, 1, 2);
+        g.addEdgeFeature(1, db.getFeature("test", "a"));
+        g.addEdge(2, 2, 3);
+        g.addEdgeFeature(2, db.getFeature("test", "a"));
+        g.addEdge(3, 1, 4);
+        g.addEdgeFeature(3, db.getFeature("test", "a"));
+        g.addEdge(4, 2, 4);
+        g.addEdgeFeature(4, db.getFeature("test", "a"));
+        return g;
+    }
+
+
+    public static Graph getMultigraph1(DBInterface db) {
+
+        MutableGraph g = new MutableGraph();
+        g.addNode(1);
+        g.addNodeFeature(1, db.getFeature("test", "a"));
+        g.addNode(2);
+        g.addNodeFeature(2, db.getFeature("test", "b"));
+        g.addNode(3);
+        g.addNodeFeature(3, db.getFeature("test", "c"));
+        g.addNode(4);
+        g.addNodeFeature(4, db.getFeature("test", "d"));
+        g.addEdge(1, 1, 2);
+        g.addEdgeFeature(1, db.getFeature("test", "a"));
+        g.addEdge(2, 2, 3);
+        g.addEdgeFeature(2, db.getFeature("test", "b"));
+        g.addEdge(3, 1, 2);
+        g.addEdgeFeature(3, db.getFeature("test", "a"));
+        g.addEdge(4, 2, 4);
+        g.addEdgeFeature(4, db.getFeature("test", "a"));
+        return g;
+    }
+
+
+	public static void assertGraphsAreIdentical(Graph g1, Graph g2) {
+		assertEquals(g1.getNodeIDs(), g2.getNodeIDs());
+		assertEquals(g1.getEdgeIDs(), g2.getEdgeIDs());
+		assertEquals(g1.getFeatures(), g2.getFeatures());
+		for(int nodeID : g1.getNodeIDs()) {
+			assertEquals(g1.getNodeFeatures(nodeID), g2.getNodeFeatures(nodeID));
+		}
+		for(int edgeID : g1.getEdgeIDs()) {
+			assertEquals(g1.getNodeFeatures(edgeID), g2.getNodeFeatures(edgeID));
+			assertEquals(g1.getEdgeSourceNodeID(edgeID), 
+				     g2.getEdgeSourceNodeID(edgeID));
+			assertEquals(g1.getEdgeTargetNodeID(edgeID), 
+				     g2.getEdgeTargetNodeID(edgeID));
+		}
+	}
+	
+
+	public static String toString(Graph g1) {
+		String ret = "nodes: " + g1.getNodeIDs() + "\n";
+		ret += "edges: " + g1.getEdgeIDs() + "\n";
+		ret += "features: " + g1.getFeatures() + "\n";
+		for(int nodeID : g1.getNodeIDs()) {
+			ret += "node #" + nodeID + ": " + g1.getNodeFeatures(nodeID);
+		}
+		for(int edgeID : g1.getEdgeIDs()) {
+			ret += "edge #" + edgeID + "(" + g1.getEdgeSourceNodeID(edgeID) + 
+					"," + g1.getEdgeTargetNodeID(edgeID) +"): " + 
+					g1.getNodeFeatures(edgeID);
+		}
+		return ret;
+	}
+
+}
