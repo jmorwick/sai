8c840444de59345fa52f63ff12a5324b73af7de1
Joey Kendall-Morwick <jbmorwick@gmail.com>
2015-01-02 22:59:41 -0500

    minor style update

--- SAI/src/sai/db/DBs.java
+++ SAI/src/sai/db/DBs.java
@@ -5,6 +5,7 @@ import sai.graph.GraphFactory;
 import sai.graph.MutableGraph;
 
 public class DBs {
+    
  public static void copyDBs(DBInterface fromDB, DBInterface toDB) {
 		GraphFactory<MutableGraph> gf = MutableGraph.getFactory();
 		for(int graphID : SAIUtil.iteratorToCollection(fromDB.getGraphIDIterator()))
--- SAI/src/sai/graph/Graph.java
+++ SAI/src/sai/graph/Graph.java
@@ -7,7 +7,7 @@ public interface Graph {
 	public Set<Integer> getNodeIDs();
 	public Set<Feature> getFeatures();
 	public Set<Feature> getNodeFeatures(int n);
-	public Set<Feature> getEdgeFeatures(int n);
+	public Set<Feature> getEdgeFeatures(int e);
 	public int getEdgeSourceNodeID(int edgeID);
 	public int getEdgeTargetNodeID(int edgeID);
 }
