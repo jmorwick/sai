c66de4daf9c06c7b36a3e7044987a039397c4396
Joey Kendall-Morwick <jbmorwick@gmail.com>
2015-05-12 23:35:13 -0500

    added an intermediate class for easy implementation of DB wrappers added the ImmutableGraph file I forgot to add in the last commit

--- SAI/src/sai/indexing/FeatureIndexingDBWrapper.java
+++ SAI/src/sai/indexing/FeatureIndexingDBWrapper.java
@@ -1,88 +1,27 @@
 package sai.indexing;
 
-import java.util.Set;
-import java.util.stream.Stream;
-
 import sai.db.DBInterface;
+import sai.db.DBWrapper;
 import sai.graph.Feature;
 import sai.graph.Graph;
-import sai.graph.GraphFactory;
+import sai.graph.ImmutableGraph;
 import sai.graph.MutableGraph;
 
-public class FeatureIndexingDBWrapper implements DBInterface {
-
-	private final DBInterface db;
+public class FeatureIndexingDBWrapper extends DBWrapper {
 	private final FeatureIndexGenerator gen;
 	
 	public FeatureIndexingDBWrapper(DBInterface wrappedDB, FeatureIndexGenerator gen) {
-		this.db = wrappedDB;
+		super(wrappedDB);
 		this.gen = gen;
 	}
 	
-	public DBInterface getWrappedDB() { return db; }
-	
-	@Override
-	public void disconnect() {
-		db.disconnect();
-	}
-
-	@Override
-	public boolean isConnected() {
-		return db.isConnected();
-	}
-
-	@Override
-	public <G extends Graph> G retrieveGraph(int graphID, GraphFactory<G> f) {
-		return db.retrieveGraph(graphID, f);
-	}
-
-	@Override
-	public Stream<Integer> getGraphIDStream() {
-		return db.getGraphIDStream();
-	}
-
-	@Override
-	public Stream<Integer> retrieveGraphsWithFeature(Feature f) {
-		return db.retrieveGraphsWithFeature(f);
-	}
-
-	@Override
-	public Stream<Integer> retrieveGraphsWithFeatureName(String name) {
-		return db.retrieveGraphsWithFeatureName(name);
-	}
-
-	@Override
-	public Set<Integer> getHiddenGraphs() {
-		return db.getHiddenGraphs();
-	}
-
-	@Override
-	public void hideGraph(int graphID) {
-		db.hideGraph(graphID);
-	}
-
-	@Override
-	public void unhideGraph(int graphID) {
-		db.unhideGraph(graphID);
-	}
-
-	@Override
-	public void deleteGraph(int graphID) {
-		db.deleteGraph(graphID);
-	}
-
 	@Override
 	public int addGraph(Graph g) {
 		MutableGraph g1 = new MutableGraph(g);
 		for(Feature f : gen.apply(g))
 			g1.addFeature(f);
 			
-		return db.addGraph(g1);
-	}
-
-	@Override
-	public int getDatabaseSize() {
-		return db.getDatabaseSize();
+		return getWrappedDB().addGraph(new ImmutableGraph(g1));
 	}
 
 }
