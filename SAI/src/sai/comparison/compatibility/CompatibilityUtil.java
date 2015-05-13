ff362c7c8687ffc467a6f0be77e497be993dc06b
Joey Kendall-Morwick <jbmorwick@gmail.com>
2015-05-12 21:07:42 -0500

    updated CompatabilityUtil to use streams

--- SAI/src/sai/comparison/compatibility/CompatibilityUtil.java
+++ SAI/src/sai/comparison/compatibility/CompatibilityUtil.java
@@ -1,12 +1,10 @@
 package sai.comparison.compatibility;
 
-import info.kendall_morwick.funcles.Funcles;
-import info.kendall_morwick.funcles.tuple.Pair;
-
 import java.util.Set;
 
 import sai.graph.Feature;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 
 public class CompatibilityUtil {
@@ -17,48 +15,40 @@ public class CompatibilityUtil {
 						a.getValue().equals(b.getValue());
 	}
 	
-	
     /** checks if each feature in set 1 is compatible with at least one
      * feature in set 2, allowing repeats.
      */
 	public static FeatureSetCompatibilityChecker greedy1To1Checker(
 			final FeatureCompatibilityChecker featureComp) {
-		return (t1s, t2s) -> { // TODO: use streams below
-	        t2s = Sets.newHashSet(t2s);
-	        for(Feature t1 : t1s) {
-	            boolean foundMatch = false;
-	            for(Feature t2 : t2s) {
-	                if(Funcles.apply(featureComp, t1, t2)) {
-	                    foundMatch = true;
-	                    t2s.remove(t2);
-	                    break;
-	                }
-	            }
-	            if(!foundMatch) return false;
-	        }
-	        return true;
+		return (t1s, t2s) -> { 
+	        Set<Feature> t2sThreadSafe = Sets.newConcurrentHashSet(t2s);
+	        return t1s.stream().allMatch(
+	        		f1 -> {
+	        			return Sets.newHashSet(t2sThreadSafe).stream()
+	        			.anyMatch( f2 -> {
+	        				if(featureComp.test(f1, f2)) {
+	        					t2sThreadSafe.remove(f2);
+	        					return true;
+	        				}
+	        				return false;
+	        			});
+	        		}
+	        );
 	    };
 	}
-	
 
-    /** 
-     * returns a quadratic-worst-case feature-set comparison algorithm which 
-     * allows one feature to subsume unlimited features in the other feature set. 
+    /** checks if each feature in set 1 is compatible with at least one
+     * feature in set 2, allowing repeats.
      */
 	public static FeatureSetCompatibilityChecker many1To1Checker(
 			final FeatureCompatibilityChecker featureComp) {
-		return (t1s, t2s) -> { // TODO: use streams below
-	        for(Feature t1 : t1s) {
-	            boolean foundMatch = false;
-	            for(Feature t2 : t2s) {
-	                if(Funcles.apply(featureComp, t1, t2)) {
-	                    foundMatch = true;
-	                    break;
-	                }
-	            }
-	            if(!foundMatch) return false;
-	        }
-	        return true;
-		};
+		return (t1s, t2s) -> { 
+	        Set<Feature> t2sThreadSafe = ImmutableSet.copyOf(t2s);
+	        return t1s.stream().allMatch(
+	        		f1 -> t2sThreadSafe.stream()
+	        			.anyMatch(f2 -> featureComp.test(f1, f2))
+	        );
+	    };
 	}
+	
 }
