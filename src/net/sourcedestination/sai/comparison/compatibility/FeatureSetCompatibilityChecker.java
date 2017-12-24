6bbc08ea71c0d6884cc7d69597f53dc71b73e1fd
jmorwick <jbmorwick@gmail.com>
2017-12-24 11:16:43 -0600

    update compatibility checker to use streams instead of collections

--- src/net/sourcedestination/sai/comparison/compatibility/FeatureSetCompatibilityChecker.java
+++ src/net/sourcedestination/sai/comparison/compatibility/FeatureSetCompatibilityChecker.java
@@ -1,49 +1,60 @@
 package net.sourcedestination.sai.comparison.compatibility;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import net.sourcedestination.funcles.predicate.Predicate2;
 import net.sourcedestination.sai.graph.Feature;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Sets;
+public interface FeatureSetCompatibilityChecker extends Predicate2<Stream<Feature>,Stream<Feature>> {
 
-public interface FeatureSetCompatibilityChecker extends Predicate2<Set<Feature>,Set<Feature>> {
-
-    /** checks if each feature in set 1 is compatible with at least one
-     * feature in set 2, allowing repeats.
-     */
-	public static FeatureSetCompatibilityChecker greedy1To1Checker(
-			final FeatureCompatibilityChecker featureComp) {
-		return (t1s, t2s) -> { 
-	        Set<Feature> t2sThreadSafe = Sets.newConcurrentHashSet(t2s);
-	        return t1s.stream().allMatch(
-	        		f1 -> {
-	        			return Sets.newHashSet(t2sThreadSafe).stream()
-	        			.anyMatch( f2 -> {
-	        				if(featureComp.test(f1, f2)) {
-	        					t2sThreadSafe.remove(f2);
-	        					return true;
-	        				}
-	        				return false;
-	        			});
-	        		}
-	        );
-	    };
+	/** checks if each feature in set 1 is compatible with at least one
+	 * feature in set 2, allowing repeats. fs2 is read and stored in memory.
+	 * Complexity:  O(|fs1| * |fs2|)
+	 */
+	public static boolean checkFeaturesGreedyManyTo1(FeatureCompatibilityChecker checker,
+													 Stream<Feature> fs1, Stream<Feature> fs2) {
+		Set<Feature> fs2Set = fs2.collect(Collectors.toSet());
+		return fs1.allMatch(f1 -> fs2Set.stream()
+				.anyMatch( f2 -> checker.apply(f1, f2)));
 	}
-	
 
-    /** checks if each feature in set 1 is compatible with at least one
-     * feature in set 2, allowing repeats.
-     */
-	public static FeatureSetCompatibilityChecker many1To1Checker(
-			final FeatureCompatibilityChecker featureComp) {
-		return (t1s, t2s) -> { 
-	        Set<Feature> t2sThreadSafe = ImmutableSet.copyOf(t2s);
-	        return t1s.stream().allMatch(
-	        		f1 -> t2sThreadSafe.stream()
-	        			.anyMatch(f2 -> featureComp.test(f1, f2))
-	        );
-	    };
+	/** checks if each feature in set 1 is the same as at least one
+	 * feature in set 2, allowing repeats. fs2 is read and stored in memory.
+	 * Complexity: O(|fs1| * ln(|fs2|))
+	 */
+	public static boolean checkFeaturesGreedyManyTo1(Stream<Feature> fs1, Stream<Feature> fs2) {
+		Set<Feature> fs2Set = fs2.collect(Collectors.toSet());
+		return fs1.allMatch(f1 -> fs2Set.contains(fs1));
+	}
+
+	/** checks if each feature in set 1 is compatible with exactly one
+	 * feature in set 2, allowing repeats. fs2 is read and stored in memory.
+	 * Complexity:  O(|fs1| * |fs2|)
+	 */
+	public static boolean checkFeaturesGreedy1To1(FeatureCompatibilityChecker checker,
+													 Stream<Feature> fs1, Stream<Feature> fs2) {
+		Set<Feature> fs2Set = fs2.collect(Collectors.toSet());
+		return fs1.allMatch(f1 -> {
+			int fs2Size = fs2Set.size();
+			fs2Set.removeIf( f2 -> checker.apply(f1, f2));
+			return fs2Size > fs2Set.size();
+		});
+	}
+
+	/** checks if each feature in set 1 is the same as exactly one
+	 * feature in set 2, allowing repeats. fs2 is read and stored in memory.
+	 * Complexity:  O(|fs1| * ln(|fs2|))
+	 */
+	public static boolean checkFeaturesGreedy1To1(Stream<Feature> fs1, Stream<Feature> fs2) {
+		Set<Feature> fs2Set = fs2.collect(Collectors.toSet());
+		return fs1.allMatch(f1 -> {
+			if(fs2Set.contains(f1)) {
+				fs2Set.remove(f1);
+				return true;
+			}
+			return false;
+		});
 	}
 }
