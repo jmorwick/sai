8332f9565d981aa17a8e0a8f724e13dd304d5581
Alex Morehead <alex.morehead@gmail.com>
2018-02-09 22:36:11 -0600

    Redesigned Independent SAI Metrics
    Made a new IndepedentDBStatistic class for new SAI metrics to extend. This class allows for graphs to be analyzed on an individual level.

--- src/net/sourcedestination/sai/reporting/stats/IndependentDBStatistic.java
+++ src/net/sourcedestination/sai/reporting/stats/IndependentDBStatistic.java
@@ -1,46 +1,40 @@
 package net.sourcedestination.sai.reporting.stats;
 
 import net.sourcedestination.sai.db.DBInterface;
-import net.sourcedestination.sai.graph.Feature;
 import net.sourcedestination.sai.graph.Graph;
-import net.sourcedestination.sai.graph.GraphFactory;
-import net.sourcedestination.sai.graph.ImmutableGraph;
 import net.sourcedestination.sai.task.Task;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
 
-/** A DB statistic that can be calculated by examining each graph, independently, in parallel.
- * Created by jmorwick on 7/7/17.
+/**
+ * A DB statistic template interface that can be
+ * implemented by DB statistic subclasses in order
+ * to apply a customized "processGraph" method to
+ * every graph in a given database.
+ * Created by amorehead on 2/7/18.
  */
+
 public interface IndependentDBStatistic extends DBStatistic {
 
-    public void processGraph(Graph g);
-    public double getResult();
+    // The following represents a placeholder method to be overridden in subclasses.
+    public double processGraph(Graph g);
 
     public default Task<Double> apply(DBInterface db) {
-        return new Task<Double> () {
+        IndependentDBStatistic self = this;
+        return new Task<Double>() {
             private final AtomicInteger count = new AtomicInteger();
 
             public Double get() {
                 int dbsize = db.getDatabaseSize();
-                Set<Feature> features = new HashSet<>();
-                db.getGraphIDStream()
-                        .forEach(id -> {
-                            Graph g = db.retrieveGraph(id);
-                            processGraph(g);
-                            count.incrementAndGet();
-                        });
-                if(dbsize > 0) return getResult() / dbsize;
+                long total = db.getGraphIDStream()
+                        .map(id -> db.retrieveGraph(id))
+                        .peek(g -> count.incrementAndGet()) // This increments the progress counter.
+                        .map(self::processGraph)
+                        .count();
+                if (dbsize > 0) return (double) total / dbsize;
                 return 0.0;
             }
 
-            @Override
-            public double getPercentageDone() {
-                return (double)count.get() / (db.getDatabaseSize());
-            }
         };
     }
 }
