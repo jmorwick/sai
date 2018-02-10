6436ac76de523528872ff4211d8b4dc3f9b7c426
Alex Morehead <alex.morehead@gmail.com>
2018-02-09 22:43:46 -0600

    Made New SAI Metric
    Created the new PercentageOfIsolatedPoints class based on IndependentDBStatistic as well as its accompanying test class.

---
+++ src/net/sourcedestination/sai/reporting/stats/PercentageOfIsolatedPoints.java
@@ -0,0 +1,21 @@
+package net.sourcedestination.sai.reporting.stats;
+
+import net.sourcedestination.sai.graph.Graph;
+
+/* A DB statistic that finds the ratio of isolated nodes, those with
+ degree zero, to the total number of nodes in the entire graph.
+ Created by amorehead on 2/9/18. */
+public class PercentageOfIsolatedPoints implements IndependentDBStatistic {
+
+    public double processGraph(Graph g) {
+        int totalNodes = ((int) g.getNodeIDs().count());
+        // The following represents the processing of a stream of integers.
+        return totalNodes == 0 ? 0.0 :
+                g.getNodeIDs()
+                        /* The following finds the quotient of dividing the number
+                        of nodes with degree zero by the total number of nodes. */
+                        .filter(nid -> (double) g.getIncidentEdges(nid).count() == 0)
+                        .count() / (double) totalNodes;
+    }
+
+}
