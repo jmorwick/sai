ed9494ed75bb5a719ff182cae1cc8707fba0428e
Alex Morehead <alex.morehead@gmail.com>
2018-02-09 22:33:22 -0600

    Redesigned SAI Metrics
    Restructured the IndependentDBStatistic class into the ComprehensiveDBStatistic class as it still applies the Task method of its subclasses over entire databases of graphs at one time. This will now act as a legacy class.

---
+++ src/net/sourcedestination/sai/reporting/stats/ComprehensiveDBStatistic.java
@@ -0,0 +1,44 @@
+package net.sourcedestination.sai.reporting.stats;
+
+import net.sourcedestination.sai.db.DBInterface;
+import net.sourcedestination.sai.graph.Feature;
+import net.sourcedestination.sai.graph.Graph;
+import net.sourcedestination.sai.task.Task;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* A DB statistic that can be calculated by examining
+ every graph in a given database in parallel.
+ Created by amorehead on 1/31/18. */
+public interface ComprehensiveDBStatistic extends DBStatistic {
+
+    public double processGraph(Graph g);
+
+    public double getResult();
+
+    public default Task<Double> apply(DBInterface db) {
+        return new Task<Double>() {
+            private final AtomicInteger count = new AtomicInteger();
+
+            public Double get() {
+                int dbsize = db.getDatabaseSize();
+                Set<Feature> features = new HashSet<>();
+                db.getGraphIDStream()
+                        .forEach(id -> {
+                            Graph g = db.retrieveGraph(id);
+                            processGraph(g);
+                            count.incrementAndGet();
+                        });
+                if (dbsize > 0) return getResult() / dbsize;
+                return 0.0;
+            }
+
+            @Override
+            public double getPercentageDone() {
+                return (double) count.get() / (db.getDatabaseSize());
+            }
+        };
+    }
+}
