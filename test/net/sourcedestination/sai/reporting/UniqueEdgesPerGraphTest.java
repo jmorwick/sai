42c60d665be1250d1985bc20ccdbc90fc8697aa2
Alex Morehead <alex.morehead@gmail.com>
2018-02-09 22:39:02 -0600

    Redesigned SAI Metric
    Restructured the UniqueEdgesPerGraph class based on IndependentDBStatistic and also wrote a test for the class.

---
+++ test/net/sourcedestination/sai/reporting/UniqueEdgesPerGraphTest.java
@@ -0,0 +1,30 @@
+package net.sourcedestination.sai.reporting;
+
+import net.sourcedestination.sai.db.DBInterface;
+import net.sourcedestination.sai.db.SampleDBs;
+import net.sourcedestination.sai.reporting.stats.UniqueEdgesPerGraph;
+import org.junit.Test;
+
+import java.nio.file.AccessDeniedException;
+
+import static org.junit.Assert.assertEquals;
+
+/* This is a test class for "UniqueEdgesPerGraph". It passes if and only
+ if the statistic is fully accurate on the small graph database provided.
+ Created by amorehead on 2/7/2018. */
+public class UniqueEdgesPerGraphTest {
+
+    @Test
+    public void testLexicalCompatibility() throws AccessDeniedException {
+
+        /* This is a method that tests whether or not the
+         "UniqueEdgesPerGraph" class is implemented correctly or not. */
+        DBInterface db = SampleDBs.smallGraphsDB();
+
+        // This allows us to test the statistic's class.
+        UniqueEdgesPerGraph stat = new UniqueEdgesPerGraph();
+
+        // The last parameter for "assertEquals" represents the amount of error allowed for the test results.
+        assertEquals(4, stat.processGraph(db.retrieveGraph(1)), 0);
+    }
+}
