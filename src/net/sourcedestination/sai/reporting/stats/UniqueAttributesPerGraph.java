d69c66e534ea5f70e2b09b249b4de194870e0893
Alex Morehead <alex.morehead@gmail.com>
2018-02-09 22:37:48 -0600

    Redesigned SAI Metric
    Restructured the UniqueAttributesPerGraph class based on IndependentDBStatistic and wrote a test class for it as well.

--- src/net/sourcedestination/sai/reporting/stats/UniqueAttributesPerGraph.java
+++ src/net/sourcedestination/sai/reporting/stats/UniqueAttributesPerGraph.java
@@ -1,23 +1,14 @@
 package net.sourcedestination.sai.reporting.stats;
 
-import com.google.common.collect.Sets;
-import net.sourcedestination.sai.db.DBInterface;
 import net.sourcedestination.sai.graph.Feature;
 import net.sourcedestination.sai.graph.Graph;
-import net.sourcedestination.sai.graph.GraphFactory;
-import net.sourcedestination.sai.graph.ImmutableGraph;
-import net.sourcedestination.sai.task.Task;
 
-import java.util.HashSet;
-import java.util.OptionalDouble;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
 
-/**  TODO: add test
- *   TODO: comment / license
- */
+/* A DB statistic that can be used to calculate the
+ number of unique attributes in a given database of graphs.
+ Created by amorehead on 1/31/18. */
 public class UniqueAttributesPerGraph implements IndependentDBStatistic {
 
     private final Set<String> featureNames;
@@ -27,6 +18,8 @@ public class UniqueAttributesPerGraph implements IndependentDBStatistic {
         this.discoveredFeatures = ConcurrentHashMap.newKeySet();
         this.featureNames = null;
     }
+
+    // The following are no longer needed for the current implementation of this class.
     public UniqueAttributesPerGraph(Set<String> featureNames) {
         this.discoveredFeatures = ConcurrentHashMap.newKeySet();
         this.featureNames = featureNames;
@@ -34,10 +27,16 @@ public class UniqueAttributesPerGraph implements IndependentDBStatistic {
 
     public boolean isFeatureOfInterest(Feature f) {
         return featureNames == null ||  // all features are of interest
-            featureNames.contains(f.getName());   // it's included in the feature names of interest
+                featureNames.contains(f.getName());   // it's included in the feature names of interest
     }
 
-    public void processGraph(Graph g) {
+    // This returns the result we are looking for.
+    public double getResult() {
+        return discoveredFeatures.size();
+    }
+
+    // This method represents the main functionality of this class.
+    public double processGraph(Graph g) {
         g.getFeatures().filter(this::isFeatureOfInterest)
                 .forEach(discoveredFeatures::add);
         g.getNodeIDs()
@@ -51,9 +50,6 @@ public class UniqueAttributesPerGraph implements IndependentDBStatistic {
                 .forEach(s -> s
                         .filter(this::isFeatureOfInterest)
                         .forEach(discoveredFeatures::add));
-    }
-
-    public double getResult() {
-        return discoveredFeatures.size();
+        return getResult();
     }
 }
---
+++ test/net/sourcedestination/sai/reporting/UniqueAttributesPerGraphTest.java
@@ -0,0 +1,31 @@
+package net.sourcedestination.sai.reporting;
+
+import net.sourcedestination.sai.db.DBInterface;
+import net.sourcedestination.sai.db.SampleDBs;
+import net.sourcedestination.sai.reporting.stats.UniqueAttributesPerGraph;
+import org.junit.Test;
+
+import java.nio.file.AccessDeniedException;
+
+import static org.junit.Assert.assertEquals;
+
+/* This is a test class for "UniqueAttributesPerGraph". It passes if and only
+ if the statistic is fully accurate on the small graph database provided.
+ Written by amorehead on 2/9/2018. */
+public class UniqueAttributesPerGraphTest {
+
+    @Test
+    public void testLexicalCompatibility() throws AccessDeniedException {
+
+        /* This is a method that tests whether or not the
+         "UniqueAttributesPerGraph" class is implemented correctly or not. */
+        DBInterface db = SampleDBs.smallGraphsDB();
+
+        // This allows us to test the statistic's class.
+        UniqueAttributesPerGraph stat = new UniqueAttributesPerGraph();
+
+        // The last parameter for "assertEquals" represents the amount of error allowed for the test results.
+        assertEquals(5, stat.processGraph(db.retrieveGraph(1)), 0);
+    }
+
+}
