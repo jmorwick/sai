44440bc107dd01e45e9eab06617389ffdad2ecbe
Alex Morehead <alex.morehead@gmail.com>
2018-02-09 22:42:06 -0600

    Made New SAI Metric
    Created the new AverageDegreePerGraph class based on IndependentDBStatistic as well as its accompanying test class.

---
+++ test/net/sourcedestination/sai/reporting/AverageDegreePerGraphTest.java
@@ -0,0 +1,30 @@
+package net.sourcedestination.sai.reporting;
+
+import net.sourcedestination.sai.db.DBInterface;
+import net.sourcedestination.sai.db.SampleDBs;
+import net.sourcedestination.sai.reporting.stats.AverageDegreePerGraph;
+import org.junit.Test;
+
+import java.nio.file.AccessDeniedException;
+
+import static org.junit.Assert.assertEquals;
+
+/* This is a test class for "AverageDegreePerGraph". It passes if and only
+ if the statistic is fully accurate on the small graph database provided.
+ Written by amorehead on 2/3/2018. */
+public class AverageDegreePerGraphTest {
+
+    @Test
+    public void testLexicalCompatibility() throws AccessDeniedException {
+
+        /* This is a method that tests whether or not the
+         "AverageDegreePerGraph" class is implemented correctly or not. */
+        DBInterface db = SampleDBs.smallGraphsDB();
+
+        // This allows us to test the statistic's class.
+        AverageDegreePerGraph stat = new AverageDegreePerGraph();
+
+        // The last parameter for "assertEquals" represents the amount of error allowed for the test results.
+        assertEquals(2, stat.processGraph(db.retrieveGraph(1)), 0);
+    }
+}
