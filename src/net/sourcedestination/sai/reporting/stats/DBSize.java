fde28962ed36553685dcbb399e78121781a01bc0
amorehead <alex.morehead@gmail.com>
2018-02-12 12:28:58 -0600

    Updated Code Formatting
    Refactored code in SAI's metrics.

--- src/net/sourcedestination/sai/reporting/stats/DBSize.java
+++ src/net/sourcedestination/sai/reporting/stats/DBSize.java
@@ -1,16 +1,10 @@
 package net.sourcedestination.sai.reporting.stats;
 
 import net.sourcedestination.sai.db.DBInterface;
-import net.sourcedestination.sai.graph.GraphFactory;
-import net.sourcedestination.sai.graph.ImmutableGraph;
-import net.sourcedestination.sai.task.Task;
-
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 
 public class DBSize extends FastDBStatistic {
     public double computeStat(DBInterface db) {
-        return (double)db.getDatabaseSize();
+        return (double) db.getDatabaseSize();
     }
 }
--- src/net/sourcedestination/sai/reporting/stats/GraphFilterStatistic.java
+++ src/net/sourcedestination/sai/reporting/stats/GraphFilterStatistic.java
@@ -3,14 +3,11 @@ package net.sourcedestination.sai.reporting.stats;
 import net.sourcedestination.sai.db.DBInterface;
 import net.sourcedestination.sai.graph.Feature;
 import net.sourcedestination.sai.graph.Graph;
-import net.sourcedestination.sai.graph.GraphFactory;
-import net.sourcedestination.sai.graph.ImmutableGraph;
 import net.sourcedestination.sai.task.Task;
 
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 /**
@@ -22,7 +19,7 @@ public interface GraphFilterStatistic extends DBStatistic {
 
     public default Task<Double> apply(DBInterface db) {
         final Predicate<Graph> filter = this::filterGraph;
-        return new Task<Double> () {
+        return new Task<Double>() {
             private final AtomicInteger count = new AtomicInteger();
 
             public Double get() {
@@ -33,13 +30,13 @@ public interface GraphFilterStatistic extends DBStatistic {
                         .peek(x -> count.incrementAndGet()) // increment progress counter
                         .filter(filter)
                         .count();
-                if(dbsize > 0) return (double)total / dbsize;
+                if (dbsize > 0) return (double) total / dbsize;
                 return 0.0;
             }
 
             @Override
             public double getPercentageDone() {
-                return (double)count.get() / (db.getDatabaseSize());
+                return (double) count.get() / (db.getDatabaseSize());
             }
         };
     }
