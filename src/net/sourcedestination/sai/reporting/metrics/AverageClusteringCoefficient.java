b7803659717ad6aecf993c4655f388c06d0ae0dd
amorehead <alex.morehead@gmail.com>
2018-04-13 12:50:59 -0500

    Almost finished with AverageClusteringCoefficient. Needs one small fix for matching neighboring edges.

--- src/net/sourcedestination/sai/reporting/metrics/AverageClusteringCoefficient.java
+++ src/net/sourcedestination/sai/reporting/metrics/AverageClusteringCoefficient.java
@@ -5,11 +5,15 @@ import net.sourcedestination.sai.graph.Graph;
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 /* A DB metric that computes the local average
  clustering coefficient for a given graph.
  Updated by amorehead on 4/13/18. */
 public class AverageClusteringCoefficient implements IndependentDBMetric {
+
+    // The following creates a global counter variable for later use in a for-loop.
+    public int i = 0;
 
     @Override
     public double processGraph(Graph g) {
@@ -32,18 +36,25 @@ public class AverageClusteringCoefficient implements IndependentDBMetric {
             // The following creates an empty atomic integer data structure instance.
             AtomicInteger numberOfNeighborEdgesOfInterest = new AtomicInteger();
 
-            /*
             // The following increments the number of edges between neighboring nodes of the current node "n" (if applicable).
-            for (int i = 0; i < neighborsOfN.size() - 1; i++) {
+            for (i = 0; i < neighborsOfN.size() - 1; i++) {
 
-                if ((g.getIncidentToEdges(neighborsOfN.get(i)).anyMatch(g.getIncidentFromEdges(neighborsOfN.get(i + 1)))) ||
+                // The following stores relevant streams of Integers in temporary variables.
+                Stream<Integer> edgesFromI = g.getIncidentToEdges(neighborsOfN.get(i));
+                Stream<Integer> edgesToI = g.getIncidentFromEdges(neighborsOfN.get(i + 1));
+                Stream<Integer> edgesFromNeighbor = g.getIncidentFromEdges(neighborsOfN.get(i));
+                Stream<Integer> edgesToNeighbor = g.getIncidentFromEdges(neighborsOfN.get(i));
 
-                        (g.getIncidentFromEdges(neighborsOfN.get(i)).anyMatch(g.getIncidentToEdges(neighborsOfN.get(i + 1))))) {
+                /* Currently, the following is checking each Integer in the first stream
+                 and seeing if any are equal to the **second stream in and of itself**.
+                 This needs to be revised to check to see if any edge (Integer in this case)
+                 in either of the two Integer streams are equal to each other. */
+                if (edgesFromI.anyMatch(eid -> eid.equals(edgesToI)) ||
+                        (edgesFromNeighbor.anyMatch(eid -> eid.equals(edgesToNeighbor)))) {
 
                     numberOfNeighborEdgesOfInterest.incrementAndGet();
                 }
             }
-            */
 
             // The following handles a divide-by-zero error while also calculating the clustering coefficient for a given node "n".
             double clusteringCoefficient = ((degreeOfCurrentNode != 0) && (degreeOfCurrentNode - 1 != 0))
