456c3861f294e4a2117baaec969d34852da58ff4
jmorwick <jbmorwick@gmail.com>
2018-05-31 15:40:35 -0500

    made GraphType both a predicate and a graph metric w/ a default method

--- src/net/sourcedestination/sai/analysis/metrics/types/GraphType.java
+++ src/net/sourcedestination/sai/analysis/metrics/types/GraphType.java
@@ -9,11 +9,10 @@ import java.util.function.Predicate;
  * Created by jmorwick on 7/7/17.
  */
 @FunctionalInterface
-public interface GraphType extends Predicate<Graph> {
+public interface GraphType extends GraphMetric, Predicate<Graph> {
 
-    public default GraphMetric toGraphMetric() {
-        return g -> {
-            return this.test(g) ? 1.0 : 0.0;
-        };
+    @Override
+    public default Double apply(Graph g) {
+        return test(g) ? 1.0 : 0.0;
     }
 }
