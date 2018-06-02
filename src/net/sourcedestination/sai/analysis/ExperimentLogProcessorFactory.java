7013bc1d52a9b6e3c2cc8e3f8f3e89d50c744623
jmorwick <jbmorwick@gmail.com>
2018-06-02 13:16:44 -0500

    eliminated unused interfaces

--- src/net/sourcedestination/sai/analysis/ExperimentLogProcessorFactory.java
+++
@@ -1,7 +0,0 @@
-package net.sourcedestination.sai.analysis;
-
-import java.util.function.Supplier;
-
-@FunctionalInterface
-public interface ExperimentLogProcessorFactory extends Supplier<ExperimentLogProcessor> {
-}
