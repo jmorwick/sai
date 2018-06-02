88e6b27ce7659108ff1a27473d94341d13235e16
jmorwick <jbmorwick@gmail.com>
2018-06-02 13:20:15 -0500

    generalized how logs are returned
    don’t have to be in memory, could be loaded from files

--- src/net/sourcedestination/sai/reporting/logging/InteractiveAppender.java
+++ src/net/sourcedestination/sai/reporting/logging/InteractiveAppender.java
@@ -3,13 +3,14 @@ package net.sourcedestination.sai.reporting.logging;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 
-import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public abstract class InteractiveAppender extends AppenderBase<ILoggingEvent> {
 
     public abstract void startListening();
 
-    public abstract List<String> stopListening();
+    public abstract Supplier<Stream<String>> stopListening();
 
     public abstract boolean isListening();
 }
