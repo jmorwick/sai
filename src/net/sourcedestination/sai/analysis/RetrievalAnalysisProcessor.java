247575e96de0a46920d39cc36b74ff62f9bf1836
jmorwick <jbmorwick@gmail.com>
2018-06-10 17:37:25 -0500

    refactored factory for retrieval processor

--- src/net/sourcedestination/sai/analysis/RetrievalAnalysisProcessor.java
+++ src/net/sourcedestination/sai/analysis/RetrievalAnalysisProcessor.java
@@ -1,8 +1,13 @@
 package net.sourcedestination.sai.analysis;
 
+import net.sourcedestination.funcles.tuple.Tuple3;
+import net.sourcedestination.sai.db.DBInterface;
+import net.sourcedestination.sai.experiment.retrieval.Retriever;
+
+import java.util.Arrays;
 import java.util.Map;
 
-public class RetrievalAnalysisProcessor {
+public class RetrievalAnalysisProcessor implements ExperimentLogProcessor {
 
     public static String DATE_PATTERN = "(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d) (\\d\\d):(\\d\\d):(\\d\\d).(\\d\\d\\d)";
     public static String ISSUING_QUERY_PATTERN = "(issuing query \\#(\\d+))";
@@ -11,24 +16,28 @@ public class RetrievalAnalysisProcessor {
     public static String RETRIEVED_GRAPH_PATTERN =
             "(retrieved graph \\#(\\d+)( from ([a-zA-Z0-9-]+)?( for query \\#(\\d+))?)";
 
-    static ExperimentLogProcessor retrievalTimeFactory() {
-        return new ExperimentLogProcessor() {
-            @Override
-            public String getPattern() {
-                return "("+DATE_PATTERN+")?("+
-                            ISSUING_QUERY_PATTERN+"|"+
-                            RETRIEVED_GRAPH_PATTERN+")";
-            }
 
-            @Override
-            public void processLogMessage(String... groups) {
-                    // TODO: find difference between query issued times and response times
-            }
-
-            @Override
-            public Map<String, Object> get() {
-                return null; // TODO: inidividual times for each query and overall time
-            }
-        };
+    static ExperimentLogProcessor.Factory<RetrievalAnalysisProcessor>
+    getFactory() {
+        return () -> new RetrievalAnalysisProcessor();
     }
+
+    @Override
+    public String getPattern() {
+        return "("+DATE_PATTERN+")?("+
+                ISSUING_QUERY_PATTERN+"|"+
+                RETRIEVED_GRAPH_PATTERN+")";
+    }
+
+    @Override
+    public void processLogMessage(String... groups) {
+        // TODO: find difference between query issued times and response times
+        // DEBUG: Retriever.logger.info(Arrays.toString(groups));
+    }
+
+    @Override
+    public Map<String, Object> get() {
+        return null; // TODO: inidividual times for each query and overall time
+    }
+
 }
